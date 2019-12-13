b9221c7f369750547b78a35dfa2cfe2a71013ac3
ECSTA7Y <40350823+ECSTA7Y@users.noreply.github.com>
2019-12-13 12:54:57 +0800

    update intro

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 + A text classification [Shiny App](https://shiny.rstudio.com/)  using Naive Bayes
 
++ [English Version](https://github.com/ECSTA7Y/txtnb_en)
+
 + 朴素贝叶斯垃圾短信分类 [Shiny App](https://shiny.rstudio.com/) 
 
 本程序由[宋骁](https://xsong.ltd/)编写。  
@@ -9,7 +11,7 @@
 本程序使用5567条[英文短信 ](https://www.kaggle.com/team-ai/spam-text-message-classification)作为训练集生成朴素贝叶斯分类器。  
 
 用户需要输入一段短信内容文本。下方将返回该文本的分类结果。
-由于训练文本为英文，请输入英文文本。  
+由于训练文本为英文，请输入英文文本。英文版点[这里](https://github.com/ECSTA7Y/txtnb_en)
 
 如:
 
@@ -23,4 +25,14 @@
 
 文件夹中的`naiveBayes.rds`为预先训练好的模型。
 
++ 如何使用？
 
+在你的本地的R语言安装`shiny`包之后，键入如下代码：
+
+```
+library(shiny)
+runGitHub( "txtnb", "ECSTA7Y")
+
+```
+
+即可下载和运行本App。
--- ui.R
+++ ui.R
@@ -4,8 +4,18 @@ fluidPage(
   helpText('本程序使用5567条',a('英文短信',href='https://www.kaggle.com/team-ai/spam-text-message-classification',target='_blank'),'作为训练集生成朴素贝叶斯分类器。'),
   helpText('用户需要输入一段短信内容文本。下方将返回该文本的分类结果。'),
   helpText('由于训练文本为英文，请输入英文文本。'),
-  helpText('如“Did you catch the bus ? Are you frying an egg ? Did you make a tea? Are you eating your mom\'s left over dinner ?（非垃圾短信）'),
+  
+  helpText('如:  Did you catch the bus ? Are you buying an egg ? Did you make a coffee? Are you eating your mom\'s left over lunch ?（非垃圾短信）'),
+  helpText('Yes. I will check all classes befor activities（非垃圾短信）'),
+  helpText('Jim and I are going to the NBA and having some guys. I mean games! Give me a call when ya finish.（非垃圾短信）'),
+  
+  
   helpText('We tried to call you re your reply to our sms for a free nokia mobile. Please call now 08000930705 for delivery tomorrow（垃圾短信）'),
+  helpText('BangBabes Ur order is on the way. U SHOULD receive a Service Msg 2 download UR content. If U do not, GoTo wap. bangb. tv on UR mobile internet/service menu
+（垃圾短信）'),
+  helpText('Attention! Adult 18 Content Your PXXN video will go with you quickly
+（垃圾短信）'),
+  
   helpText('程序包支持：tm、e1071'),
   textInput("sms", '短信文本输入','请输入短信文本(英文)...'),
   #verbatimTextOutput('out', placeholder = F),
